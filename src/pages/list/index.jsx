b056d2ac2438d595e5a703ba272bf641ec0ac3a8
aoy007 <aoy007@qq.com>
2025-04-29 17:21:38 +0800

    update

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+
 export const ListPage = () => {
+  useEffect(() => {
+    (async function () {
+      const res = await (await fetch(`/api/list`)).json();
+      console.log('>>> list res', res);
+    })();
+  }, []);
   return (
     <>
       <h1>ListPage</h1>
       <p>This is the list page.</p>
     </>
   );
-}
+};
